d44a3fba4fbe419c1fe5ae2d99c80f8b56d5903c
programmerlab <kroy.iips@gmail.com>
2018-09-13 16:48:25 +0530

    initial setup

---
+++ Modules/Website/Resources/views/web/home/footer.blade.php
@@ -0,0 +1,61 @@
+
+  <!--footer-->
+ 
+  
+            
+    <footer>
+        <div class="container">
+            <div class="footer-box">
+                <h4>Ok4homes</h4>
+                <p>{{trans('countries::home/home.add')}}: 200 East 65th Street 17NO, New York</p>
+                <p>{{trans('countries::home/home.email')}}: ok4homes@support.com<br/> {{trans('countries::home/home.phone')}}: (880)-288-3999<br/> {{trans('countries::home/home.fax_no')}}: (880)-288-3689<br/>
+                </p>
+            </div>
+            <div class="footer-box">
+                <h4>{{trans('countries::home/home.buy')}}</h4>
+                <a href="#">{{trans('countries::home/home.home_sale')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.new_list')}}</a>
+                <a href="#">{{trans('countries::home/home.recently_reduce')}}</a>
+            </div>
+            <div class="footer-box">
+                <h4>{{trans('countries::home/home.sell')}}</h4>
+                <a href="#">{{trans('countries::home/home.home_sale')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.new_list')}}</a>
+                <a href="#">{{trans('countries::home/home.recently_reduce')}}</a>
+            </div>
+            <div class="footer-box">
+                <h4>My ok4homes</h4>
+                <a href="#">{{trans('countries::home/home.home_sale')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.op_house')}}</a>
+                <a href="#">{{trans('countries::home/home.new_list')}}</a>
+                <a href="#">{{trans('countries::home/home.recently_reduce')}}</a>
+            </div>
+            <div class="footer-box">
+                <h4>{{trans('countries::home/home.follow')}}</h4>
+                <ul class="social">
+                    <li><a href="#"><img src="{{asset('public/web/images/fb.png')}}"></a></li>
+                    <li><a href="#"><img src="{{asset('public/web/images/twtter.png')}}"></a></li>
+                    <li><a href="#"><img src="{{asset('public/web/images/g-plus.png')}}"></a></li>
+                    <li><a href="#"><img src="{{asset('public/web/images/youtube.png')}}"></a></li>
+                </ul>
+                <div class="grid-list">
+                    <p>© 2018 ok4homes Real Estate. All Right Reserved.</p>
+                    <a href="#">Facebook</a> &nbsp;&nbsp;| &nbsp;&nbsp;
+                    <a href="#">Sitemap</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+  
+     <script src="http://js.maxmind.com/js/apis/geoip2/v2.1/geoip2.js" type="text/javascript"></script>
+    <script src="{{asset('public/web/js/custom.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
+
+  <!--/footer-->
+ 
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App;
+use Session;
+use Modules\Countries\Entities\Countrylangs;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Carbon\Carbon::setLocale(config('app.locale'));
+
+        $subdomain = url('/');
+
+        $defaultUrl = env('BASE_URL');
+
+      //  dd(Session::flush());//
+
+        $subdomain = str_replace(array('http://','https://','https://www.','http://www.',".".$defaultUrl), "", $subdomain);
+
+        switch ($subdomain) {
+            case 'india':
+                \Carbon\Carbon::setLocale(config('app.locale'));
+                \App::setLocale('en');
+
+                break;
+            case 'thailand':
+                \Carbon\Carbon::setLocale(config('app.locale'));
+                 \App::setLocale('th');
+            default:
+                \Carbon\Carbon::setLocale(config('app.locale'));
+                \App::setLocale('en');
+                break;
+        }
+       
+        $countryLanguage = Countrylangs::with('languages')->where('created_country_id',1)->where(array('is_active'=>'1','isDefault'=>1))->first();
+
+        // dd($countryLanguage);
+
+        // $CountryArr=array();
+   
+        // $CountryArr['id']=$userCountry->id;
+        // $CountryArr['name']=$userCountry->name;
+        // $CountryArr['flag']=$userCountry->flag;
+        // $CountryArr['code']=$userCountry->code;
+        // $CountryArr['callingCodes']=$userCountry->callingCodes;
+        // $CountryArr['created_country_id']=$userCountry['countries']->id;
+        // $CountryArr['symbol']=$userCountry->symbol;
+
+        // Session::put('fcountry',$CountryArr); 
+
+        // Session::put('locale',App::getLocale());
+
+       \View::share('lang',App::getLocale());
+
+
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
